cd7ebfbcb19bcc5e52f78025449b624c6627acb3
eelnice <leedulcio@gorillac.net>
2023-10-05 16:41:41 -0400

    New config

--- J100Site/settings.py
+++ J100Site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&f+9y@z8noc022ir51-n#m$4hi3p7h+3-j7f906#5=3yl3))ho'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
